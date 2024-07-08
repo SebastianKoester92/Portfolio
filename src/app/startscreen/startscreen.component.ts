a480cfbf7a19ffa413fc2177d48a08ec9d3cc608
SebastianKoester92 <skoester.92@web.de>
2024-07-08 17:48:24 +0200

    added about me section

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { AboutmeComponent } from '../aboutme/aboutme.component';
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AboutmeComponent],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
