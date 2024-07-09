08c601266c44f8d15ae0c2ae667341cf0a773b26
SebastianKoester92 <skoester.92@web.de>
2024-07-09 18:45:49 +0200

    added skills section

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
+import { MyskillsComponent } from '../myskills/myskills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
-  imports: [NavbarComponent, AboutmeComponent],
+  imports: [NavbarComponent, AboutmeComponent, PortfolioComponent, MyskillsComponent],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
