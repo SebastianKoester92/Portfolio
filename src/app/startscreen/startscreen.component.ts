b24db3b7ab36905da4b5155ad6a0671f75356768
SebastianKoester92 <skoester.92@web.de>
2024-07-13 18:08:55 +0200

    added footer

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -5,6 +5,7 @@ import { AboutmeComponent } from '../aboutme/aboutme.component';
 import { MyskillsComponent } from '../myskills/myskills.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-startscreen',
@@ -14,6 +15,7 @@ import { ContactComponent } from '../contact/contact.component';
             PortfolioComponent,
             MyskillsComponent,
             ContactComponent,
+            FooterComponent,
           ],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
