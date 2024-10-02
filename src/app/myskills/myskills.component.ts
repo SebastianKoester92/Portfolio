147388343fae3dc7554ee838ab95fcbb4f4a52ed
SebastianKoester92 <skoester.92@web.de>
2024-10-02 05:50:35 +0200

    added scollbehavior to every button

--- src/app/myskills/myskills.component.ts
+++ src/app/myskills/myskills.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-myskills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './myskills.component.html',
   styleUrl: './myskills.component.scss'
 })
 export class MyskillsComponent {
-
+  constructor(private router: Router) {}
+  
+  scrollToId(id: string) {
+    // Navigiere zur Seite, die die Ziel-ID hat
+    this.router.navigate(['/start']).then(() => {
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
 }
